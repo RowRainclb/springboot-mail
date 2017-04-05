a5c1a61d2a681083db195d7db2f91818bf5a8b6e
clb <cuilibo@k2data.com.cn>
2017-04-05 14:40:28 +0800

    修改测试用例,去掉*.iml文件

--- src/test/java/com/k2data/k2app/service/MailServiceTest.java
+++ src/test/java/com/k2data/k2app/service/MailServiceTest.java
@@ -1,55 +1,98 @@
-package com.k2data.k2app.service;
+package com.k2data.k2app.service; 
 
 import com.k2data.k2app.validate.MailSimpleK2;
+import com.k2data.k2app.validate.MailTemplateK2;
 import org.apache.velocity.app.VelocityEngine;
 import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After; 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * 测试mail
- * Created by clb on 17-3-21.
- */
+* MailService Tester. 
+* 
+* @author <clb> 
+* @since <pre>四月 5, 2017</pre> 
+* @version 1.0 
+*/ 
 @RunWith(SpringRunner.class)
 @Rollback
+@Transactional
 @SpringBootTest
-public class MailServiceTest {
+public class MailServiceTest { 
 
-	@Autowired
-	private MailService mailService;
-	@Autowired
-	VelocityEngine velocityEngine;
+    @Autowired
+    private MailService mailService;
+    @Autowired
+    VelocityEngine velocityEngine;
 
-	private String to = "clbzxj0720@163.com";
+    private String to = "xx@163.com";
+    
+    @Before
+    public void before() throws Exception { 
+    } 
+    
+    @After
+    public void after() throws Exception { 
+    } 
+    
+        /** 
+    * 
+    * Method: initSender(MailSenderK2 mailSenderK2) 
+    * 
+    */ 
+    @Test
+    public void testInitSender() throws Exception { 
+    //TODO: Test goes here... 
+    } 
+    
+        /** 
+    * 
+    * Method: sendSimpleMail(MailSimpleK2 mail) 
+    * 
+    */ 
+    @Test
+    public void testSendSimpleMail() throws Exception {
+        MailSimpleK2 mailSimpleK2 = new MailSimpleK2();
+        mailSimpleK2.setHost("smtp.qq.com");
+        mailSimpleK2.setPort(465);
+        mailSimpleK2.setSendFrom("1029625587@qq.com");
+        mailSimpleK2.setPwd("tlamonohqvzzbbba");
+        mailSimpleK2.setSendTO("clbzxj0720@163.com");
+        mailSimpleK2.setSubject("ti");
+        mailSimpleK2.setContent("ti");
+        mailService.initSender(mailSimpleK2);
+        mailService.sendSimpleMail(mailSimpleK2);
+    } 
+    
+    /**
+    * 
+    * Method: sendHtmlMail(MailTemplateK2 mailTemplateK2) 
+    * 
+    */ 
+    @Test
+    public void testSendHtmlMail() throws Exception {
+        MailTemplateK2 templateK2 = new MailTemplateK2();
+        templateK2.setHost("smtp.qq.com");
+        templateK2.setPort(465);
+        templateK2.setSendFrom("1029625587@qq.com");
+        templateK2.setPwd("tlamonohqvzzbbba");
+        templateK2.setSendTO("clbzxj0720@163.com");
+        templateK2.setSubject("top:xxx");
+        templateK2.setTemplateName("mailTemplate.vm");
+        Map map1 = new HashMap<>();
+        map1.put("toUserName","这是客户名称");
+        map1.put("message","这是内容");
+        templateK2.setTemplateParams(map1);
+        mailService.sendHtmlMail(templateK2);
+    } 
 
-	@Test
-	public void testSimpleMain() throws Exception {
-		MailSimpleK2 mailSimpleK2 = new MailSimpleK2();
-		mailSimpleK2.setHost("smtp.qq.com");
-		mailSimpleK2.setPort(465);
-		mailSimpleK2.setSendFrom("1029625587@qq.com");
-		mailSimpleK2.setPwd("tlamonohqvzzbbba");
-		mailSimpleK2.setSendTO("clbzxj0720@163.com");
-		mailSimpleK2.setSubject("ti");
-		mailSimpleK2.setContent("ti");
-		mailService.initSender(mailSimpleK2);
-		mailService.sendSimpleMail(mailSimpleK2);
-	}
-
-	public void sendAttachmentsMail() {
-		mailService.sendAttachmentsMail(to, "主题：带附件的邮件", "有附件，请查收！", "/home/clb/shutdown.sh");
-	}
-
-	public void sendInlineResourceMail() {
-		String rscId = "rscId001";
-		mailService.sendInlineResourceMail(to,
-				"主题：嵌入静态资源的邮件",
-				"<html><body>这是有嵌入静态资源：<img src=\'cid:" + rscId + "\' ></body></html>",
-				"/home/clb/图片/选区_010.png",
-				rscId);
-	}
-
-}
+    } 
