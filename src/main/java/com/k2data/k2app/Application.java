be3226c83a8e23f7aebc9001aab005e8fe7ef3c6
clb <cuilibo@k2data.com.cn>
2017-04-10 10:41:31 +0800

    fix port

--- src/main/java/com/k2data/k2app/Application.java
+++ src/main/java/com/k2data/k2app/Application.java
@@ -3,10 +3,12 @@ package com.k2data.k2app;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Log4j2
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableTransactionManagement
 public class Application {
     
