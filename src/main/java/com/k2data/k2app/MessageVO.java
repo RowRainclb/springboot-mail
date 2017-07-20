9b7415138385e691fb1b6efb4a0e471f132c81c9
clb <cuilibo@k2data.com.cn>
2017-07-20 18:34:20 +0800

    fix

---
+++ src/main/java/com/k2data/k2app/MessageVO.java
@@ -0,0 +1,12 @@
+package com.k2data.k2app;
+
+import lombok.Data;
+
+/**
+ * Created by clb on 17-5-23.
+ */
+@Data
+public class MessageVO {
+	private String name;
+	private Integer age;
+}
--- src/test/java/com/k2data/k2app/service/MailServiceTest.java
+++ src/test/java/com/k2data/k2app/service/MailServiceTest.java
@@ -1,5 +1,6 @@
 package com.k2data.k2app.service;
 
+import com.k2data.k2app.MessageVO;
 import com.k2data.k2app.validate.MailSimpleK2;
 import com.k2data.k2app.validate.MailTemplateK2;
 import org.apache.velocity.app.VelocityEngine;
@@ -12,7 +13,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -79,18 +82,42 @@ public class MailServiceTest {
     @Test
     public void testSendHtmlMail() throws Exception {
         MailTemplateK2 templateK2 = new MailTemplateK2();
-        templateK2.setHost("smtp.qq.com");
+//        templateK2.setHost("smtp.qq.com");
+//        templateK2.setPort(465);
+//        templateK2.setSendFrom("1029625587@qq.com");
+//        templateK2.setPwd("tlamonohqvzzbbba");
+        templateK2.setHost("smtp.exmail.qq.com");
         templateK2.setPort(465);
-        templateK2.setSendFrom("1029625587@qq.com");
-        templateK2.setPwd("tlamonohqvzzbbba");
+        templateK2.setSendFrom("cuilibo@k2data.com.cn");
+        templateK2.setPwd("K2data");
+
         templateK2.setSendTO("clbzxj0720@163.com");
         templateK2.setSubject("top:xxx");
-        templateK2.setTemplateName("mailTemplate.vm");
+        templateK2.setTemplateName("waringTemplate.vm");
         Map map1 = new HashMap<>();
         map1.put("toUserName","这是客户名称");
         map1.put("message","这是内容");
+        List<MessageVO> list = new ArrayList<>();
+        MessageVO messageVO = new MessageVO();
+        messageVO.setName("cuilibo");
+        messageVO.setAge(11);
+        MessageVO messageVO1 = new MessageVO();
+        messageVO1.setName("cuilibo1");
+        messageVO1.setAge(13);
+        list.add(messageVO);
+        list.add(messageVO1);
+        map1.put("list",list);
+        String content = "<table><thead><tr><td>年龄</td><td>姓名</td></tr></thead><tbody>" ;
+        for(MessageVO messageVO2 : list){
+            content = content+ "<tr><td>" +messageVO2.getName()+
+                    "</td><td>" + messageVO2.getAge()+
+                    "</td></tr>";
+        }
+        map1.put("content",content);
         templateK2.setTemplateParams(map1);
+
+        mailService.initSender(templateK2);
         mailService.sendHtmlMail(templateK2);
     } 
 
-    } 
+}
